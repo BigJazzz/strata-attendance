020eb44b75398f7909031e03f71719b75141438b
Jarryd Pearson <j@rryd.au>
2025-07-24 14:35:13 +1000

    Add CORS support

--- api/index.js
+++ api/index.js
@@ -1,8 +1,10 @@
 // api/index.js
 import express from 'express';
+import cors from 'cors';
 import { createClient } from '@libsql/client';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 
 // --- Database Configuration ---
